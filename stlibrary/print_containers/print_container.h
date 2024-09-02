1d6d74072da19477dcc9cc6d9f2e5316c40d3ce4
Maks_Nik_CPP7 <odionyx@yandex.ru>
2024-09-02 17:18:13 +0400

    add smart_.../...

--- stlibrary/print_containers/print_container.h
+++ stlibrary/print_containers/print_container.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <iostream>
+#include <iterator>
 
 template <typename Type>
 void print_container( Type& v){
+//    copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
     for(const auto& it: v){
         std::cout<< it<< ' ';
     }
